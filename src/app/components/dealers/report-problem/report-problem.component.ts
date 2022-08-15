42328958b93b9badee73932b314b4143127f8bd7
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-08-15 16:55:42 +0100

    update

--- src/app/components/dealers/report-problem/report-problem.component.ts
+++ src/app/components/dealers/report-problem/report-problem.component.ts
@@ -21,6 +21,8 @@ export class ReportProblemComponent implements OnInit {
   formLoader = false
   responseError: any
   responseSuccess: any
+  FileSelected = 'false'
+
   constructor(
     private token: TokenStorageService,
     private postData: HttpRequestsService,
@@ -29,78 +31,77 @@ export class ReportProblemComponent implements OnInit {
 
   ngOnInit(): void {}
   fileUpload() {
+    this.FileSelected = 'true'
     this.uploadedFile = this.photo.nativeElement?.files[0]
-    console.log('file changefd', this.uploadedFile)
-
     let fileSize = this.uploadedFile.size
-    let toMb = fileSize / 1024
+    let toMb = fileSize / 1048576
 
-    console.log(toMb, 'mb')
     if (toMb > 5) {
       this.toastr.error('Maximum file size is 5Mb', 'Upload Error')
     }
   }
-  submitReport() {
-    let fileSize = this.uploadedFile.size
-    let toMb = fileSize / 1024
 
-    if (toMb < 5) {
-      this.formLoader = false
-      this.responseError = false
-      this.responseSuccess = false
-      let sub = this.subject.nativeElement.value!
-      let desc = this.description.nativeElement.value!
-      let img = this.uploadedFile!
-      if (sub && desc) {
-        this.formLoader = true
-        this.formError = false
-        let formData = {
-          subject: sub,
-          description: desc,
-          photo: img,
-          user_id: this.token.getUser().id,
-          role: '4',
-          dealer_id: this.token.getUser().account_id,
+  submitCaller() {
+    switch (this.FileSelected) {
+      case 'true':
+        let fileSize = this.uploadedFile.size
+        let toMb = fileSize / 1048576
+        if (toMb < 5) {
+          this.submitReport()
+        } else {
+          this.toastr.error('Maximum file size is 5Mb', 'Upload Error')
         }
-        console.log('formdata', sub, desc, typeof img, formData)
-        this.postData
-          .httpPostRequest('/create-report', formData)
-          .then((result: any) => {
-            this.formLoader = false
-            if (result.status) {
-              console.log('result', result, this.responseSuccess)
-              this.responseSuccess = true
-              this.subject.nativeElement.value = ''
-              this.description.nativeElement.value = ''
-              this.photo.nativeElement.value = ''
 
-              $('html, body').animate(
-                { scrollTop: $('.app-content').offset().top },
-                '500',
-              )
-            } else {
-              let error = result.message.response
-              {
-                error.photo &&
-                  this.toastr.error(`${error.photo}`, `Something went wrong`)
-              }
-              {
-                error.subject &&
-                  this.toastr.error(`${error.subject}`, `Something went wrong`)
-              }
-              {
-                error.description &&
-                  this.toastr.error(
-                    `${error.description}`,
-                    `Something went wrong`,
-                  )
-              }
-              console.log('result else', result)
-            }
-          })
-          .catch((err) => {
-            let error = err.message.response
-            console.log('erroror', err)
+        break
+
+      case 'false':
+        this.submitReport()
+        break
+
+      default:
+        break
+    }
+  }
+
+  submitReport() {
+    // let goodToGo = false
+    // let fileSize = this.uploadedFile.size
+    // let toMb = fileSize / 1048576
+
+    this.formLoader = false
+    this.responseError = false
+    this.responseSuccess = false
+    let sub = this.subject.nativeElement.value!
+    let desc = this.description.nativeElement.value!
+    let img = this.uploadedFile!
+    if (sub && desc) {
+      this.formLoader = true
+      this.formError = false
+      let formData = {
+        subject: sub,
+        description: desc,
+        photo: img,
+        user_id: this.token.getUser().id,
+        role: '4',
+        dealer_id: this.token.getUser().account_id,
+      }
+      this.postData
+        .httpPostRequest('/create-report', formData)
+        .then((result: any) => {
+          this.formLoader = false
+          if (result.status) {
+            console.log('result', result, this.responseSuccess)
+            this.responseSuccess = true
+            this.subject.nativeElement.value = ''
+            this.description.nativeElement.value = ''
+            this.photo.nativeElement.value = ''
+
+            $('html, body').animate(
+              { scrollTop: $('.app-content').offset().top },
+              '500',
+            )
+          } else {
+            let error = result.message.response
             {
               error.photo &&
                 this.toastr.error(`${error.photo}`, `Something went wrong`)
@@ -109,16 +110,34 @@ export class ReportProblemComponent implements OnInit {
               error.subject &&
                 this.toastr.error(`${error.subject}`, `Something went wrong`)
             }
-            this.toastr.error('', `Something went wrong`)
-            this.formLoader = false
-          })
-      } else {
-        this.formLoader = false
-        this.formError = true
-        console.log('else info', sub, desc, img)
-      }
+            {
+              error.description &&
+                this.toastr.error(
+                  `${error.description}`,
+                  `Something went wrong`,
+                )
+            }
+            console.log('result else', result)
+          }
+        })
+        .catch((err) => {
+          let error = err.message.response
+          console.log('erroror', err)
+          {
+            error.photo &&
+              this.toastr.error(`${error.photo}`, `Something went wrong`)
+          }
+          {
+            error.subject &&
+              this.toastr.error(`${error.subject}`, `Something went wrong`)
+          }
+          this.toastr.error('', `Something went wrong`)
+          this.formLoader = false
+        })
     } else {
-      this.toastr.error('Maximum file size is 5Mb', 'Upload Error')
+      this.formLoader = false
+      this.formError = true
+      console.log('else info', sub, desc, img)
     }
   }
 }
