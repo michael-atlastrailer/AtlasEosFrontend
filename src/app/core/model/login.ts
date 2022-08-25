8922f3732e941b7d2fa1c2a2a5827336517154c5
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-08-26 00:32:28 +0100

    update

--- src/app/core/model/login.ts
+++ src/app/core/model/login.ts
@@ -1,3 +1,7 @@
 export class Login {
-  constructor(public email: string, public password: string) {}
+  constructor(
+    public email: string,
+    public password: string,
+    public timer: string,
+  ) {}
 }
